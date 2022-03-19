fb6d1d63dba693d5b2268976b5f54cb9a3bd7b9c
Aditya Kumar Pandey <aditya141199@gmail.com>
2022-03-19 21:08:17 +0530

    Service Unit File

--- execute.sh
+++ execute.sh
@@ -3,3 +3,6 @@
 SCRIPT_PATH=$(dirname "$(realpath "$0")")
 source "$SCRIPT_PATH/scrapping/bin/activate"
 python "$SCRIPT_PATH/actlogin.py"
+
+# ln -s ~/Projects/scrapping-stuff/ /usr/local/lib/actlogin
+# ln -s “$(pwd)/actlogin.service” /etc/systemd/system/actlogin.service
